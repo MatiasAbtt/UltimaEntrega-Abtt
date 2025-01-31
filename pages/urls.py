94ce68face6c20eb395d52de1a0a90f315a21831
Matias Abtt <matiiabtt@gmail.com>
2025-01-31 15:20:01 -0300

    Correcciones

--- pages/urls.py
+++ pages/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
+from . import views
 from .views import (
     PageListView,
     PageDetailView,
     PageCreateView,
     PageUpdateView,
     PageDeleteView,
-    about_view
+    about_view, 
 )
 
 urlpatterns = [
     path('', PageListView.as_view(), name='page-list'),             # http://127.0.0.1:8000/
     path('pages/<int:pk>/', PageDetailView.as_view(), name='page-detail'),
+    #path('', views.home, name='home'),
     path('pages/create/', PageCreateView.as_view(), name='page-create'),
     path('pages/<int:pk>/update/', PageUpdateView.as_view(), name='page-update'),
     path('pages/<int:pk>/delete/', PageDeleteView.as_view(), name='page-delete'),
